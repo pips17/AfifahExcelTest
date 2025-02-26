37e8f352c50bf6b1657b906e77ddb3b27f2bbfe3
afifah <afifah@AFIFAH-NB>
2025-02-26 11:38:19 +0800

    Add project files.

---
+++ AfifahExcelTest/Models/RenameRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfifahExcelTest.Models
+{
+    public class RenameRule
+    {
+        public string SenderEmail { get; set; }
+        public string Subject { get; set; }
+        public string Folder { get; set; }
+        public string FileName { get; set; }
+        public bool IsRule { get; set; }
+        public string Remarks { get; set; }
+    }
+}
---
+++ AfifahExcelTest/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AfifahExcelTest.Models;
+using OfficeOpenXml;
+
+string fileName = "C:\\Users\\afifah\\Desktop\\C# Article\\List of File Rename.xlsx";
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+ExcelPackage package = new ExcelPackage(fileName);
+
+ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+int startLine = 3;
+
+int currentLine = startLine;
+
+List<RenameRule> ruleList = new List<RenameRule>();
+
+
+while (true)
+{
+    var senderEmail = worksheet.Cells[$"B{currentLine}"].Value;
+    var senderEmail2 = worksheet.Cells[currentLine, 2].Value;
+
+    if (senderEmail == null || senderEmail.ToString().Trim() == "")
+    {
+        break;
+    }
+
+    RenameRule theRule = new RenameRule()
+    {
+        SenderEmail = worksheet.Cells[currentLine, 2].Value.ToString(),
+        Subject = worksheet.Cells[currentLine, 3].Value?.ToString() ?? string.Empty,
+        Folder = worksheet.Cells[currentLine, 4].Value?.ToString() ?? string.Empty,
+        FileName = worksheet.Cells[currentLine, 5].Value?.ToString() ?? string.Empty,
+        Remarks = worksheet.Cells[currentLine, 7].Value?.ToString() ?? string.Empty
+    };
+
+    var isRule = worksheet.Cells[currentLine, 6].Value?.ToString() ?? null;
+
+    if (isRule == null)
+    {
+        theRule.IsRule = false;
+    }
+    else
+    {
+        theRule.IsRule = isRule == "Yes" ? true : false;
+    }
+
+    ruleList.Add(theRule);
+
+    currentLine++;
+}
+
+string niNakTestSender = "nurulhanees@kenanga.com.my";
+
+var ruleSesuai = ruleList
+    .Where(x=>x.SenderEmail == niNakTestSender)
+    .ToList();
+
+Console.ReadLine();
